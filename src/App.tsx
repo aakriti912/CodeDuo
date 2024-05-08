aea475692156401f377837521c3e9a57fa575868
Aakriti Thapa <thapa.aakriti570@gmail.com>
2024-05-08 20:51:39 +0545

    Linked components with App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-
+import SpellList from "./Components/SpellList/index";
+import SpellDetails from "./Components/SpellDetails/index";
+import Favorites from "./Components/Favorites/index";
 import localforage from "localforage";
 import "./style.css";
 
@@ -36,7 +38,59 @@ const App = () => {
     setSelectedSpell(spell);
   };
 
-  return <div className="main bg relative"></div>;
+  return (
+    <div className="main bg relative">
+      <img src={myImage} alt="bg" className="w-full h-[400px]" />
+
+      <div className="absolute top-0 left-0 w-full h-[400px] flex items-center justify-center  text-white">
+        <div className="flex w-full justify-center items-center min-h-screen">
+          <div className="content w-full pl-[600px]">
+            <h2 className="text-6xl font-bold flex justify-center items-center">
+              You can check your Spell List here!
+            </h2>
+            <h2 className="text-6xl font-bold flex justify-center items-center">
+              You can check your Spell List here!
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">
+          <strong style={{ color: "orange" }}>SPELL LIST !</strong> you can
+          check the detail of spelling by clicking on it and also can add to
+          favorite
+        </h1>
+        <SpellList spells={spells} onSelectSpell={selectSpell} />
+
+        {selectedSpell && (
+          <>
+            <div
+              className="fixed inset-0 bg-black opacity-50 z-10"
+              onClick={() => selectSpell(null)} // Click to close modal
+            />
+
+            {/* Modal content */}
+            <div className="fixed inset-0 flex justify-center items-center z-20">
+              <div className="bg-white p-6 rounded-lg shadow-lg relative">
+                <button
+                  className="absolute top-2 right-2 text-gray-700"
+                  onClick={() => selectSpell(null)} // Close modal
+                >
+                  &times; {/* Close icon */}
+                </button>
+                <SpellDetails
+                  spell={selectedSpell}
+                  onAddFavorite={addFavorite}
+                />
+              </div>
+            </div>
+          </>
+        )}
+
+        <Favorites spells={favorites} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
